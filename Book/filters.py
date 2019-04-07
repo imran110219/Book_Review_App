9a1743beae9801722d9f154d241bc78aab46e9d9
Sadman <imran110219@gmail.com>
2019-04-07 17:44:45 +0600

    django-filters learning

--- Book/filters.py
+++ Book/filters.py
@@ -1,13 +1,39 @@
 from .models import Book
 from Publication.models import Publication
+from Author.models import Author
+from Category.models import Category
 import django_filters
 from django import forms
 
 
 class BookFilter(django_filters.FilterSet):
 
+    bookname = django_filters.CharFilter(field_name='name')
+    pagenumber = django_filters.NumberFilter(field_name='no_of_page')
+
+    order = django_filters.OrderingFilter(
+        choices=(
+            ('name', 'NameASC'),
+            ('-name', 'NameDESC'),
+            # ('pagenumber', 'Page Number')
+
+        ),
+        fields={
+            'name': 'name',
+            'name': '-name',
+            # 'no_of_page': 'pagenumber'
+        },
+    )
+
     publication = django_filters.ModelMultipleChoiceFilter(queryset=Publication.objects.all(), widget=forms.CheckboxSelectMultiple)
+
+    authors = django_filters.ModelMultipleChoiceFilter(queryset=Author.objects.all(), widget=forms.CheckboxSelectMultiple)
+
+    categories = django_filters.ModelMultipleChoiceFilter(queryset=Category.objects.all(), widget=forms.CheckboxSelectMultiple)
 
     class Meta:
         model = Book
         fields = ['publication', 'authors', 'categories', ]
+        order_by = [
+            'name',
+        ]
