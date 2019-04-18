bbebc348afb6a21a61ff9c4cb60d4685cb35c6ea
Sadman <imran110219@gmail.com>
2019-04-18 16:58:53 +0600

    login complete

--- Account/views.py
+++ Account/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import (
-  authenticate,
-  get_user_model,
-  login,
-  logout,
+    authenticate,
+    get_user_model,
+    login,
+    logout,
 )
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
 from Book.models import Book
@@ -14,94 +16,114 @@ from django.db import transaction
 from .forms import UserLoginForm, UserRegisterForm, UserForm, ProfileForm
 from .models import Profile
 from django.contrib import messages
+import json
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 from social_django.models import UserSocialAuth
 
 
+# def login_view(request):
+#     print(request.user.is_authenticated)
+#     next = request.GET.get('next')
+#     title = "Login"
+#     form = UserLoginForm(request.POST or None)
+#     if form.is_valid():
+#         username = form.cleaned_data.get("username")
+#         password = form.cleaned_data.get("password")
+#         user = authenticate(username=username, password=password)
+#         login(request, user)
+#         if next:
+#             return redirect(next)
+#         return redirect("/books/")
+#     return render(request, "form.html", {"form": form, "title": title})
+
+
 def login_view(request):
-  print(request.user.is_authenticated)
-  next = request.GET.get('next')
-  title = "Login"
-  form = UserLoginForm(request.POST or None)
-  if form.is_valid():
-    username = form.cleaned_data.get("username")
-    password = form.cleaned_data.get("password")
-    user = authenticate(username=username, password=password)
-    login(request, user)
-    if next:
-      return redirect(next)
-    return redirect("/books/")
-  return render(request, "form.html", {"form": form, "title": title})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('books:home'))
+            else:
+                return HttpResponse(json.dumps({"message": "inactive"}), content_type="application/json")
+        else:
+            return HttpResponse(json.dumps({"message": "invalid"}), content_type="application/json")
+    else:
+        return render(request, 'register.html', {})
 
 
 def register_view(request):
-  # print(request.user.is_authenticated())
-  next = request.GET.get('next')
-  title = "Register"
-  form = UserRegisterForm(request.POST or None)
-  if form.is_valid():
-    user = form.save(commit=False)
-    password = form.cleaned_data.get('password1')
-    user.set_password(password)
-    user.save()
+    # print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Register"
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password1')
+        user.set_password(password)
+        user.save()
 
-    new_user = authenticate(username=user.username, password=password)
-    login(request, new_user)
-    if next:
-      return redirect(next)
-    return redirect("/")
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect("/")
 
-  context = {
-    "form": form,
-    "title": title
-  }
-  return render(request, "register.html", context)
-  # return render(request, "form.html", context)
+    context = {
+        "form": form,
+        "title": title
+    }
+    return render(request, "register.html", context)
+    # return render(request, "form.html", context)
 
 
 def logout_view(request):
-  logout(request)
-  return redirect("/books/")
+    logout(request)
+    return redirect("/books/")
 
 
 @login_required
 @transaction.atomic
 def update_profile(request):
-  title = "Update"
-  try:
-    profile = request.user.profile
-  except ObjectDoesNotExist:
-    profile = Profile(user=request.user)
-  if request.method == 'POST':
-    user_form = UserForm(request.POST, instance=request.user)
-    profile_form = ProfileForm(request.POST, instance=profile)
-    if user_form.is_valid() and profile_form.is_valid():
-      user_form.save()
-      profile_form.save()
-      messages.success(request, 'Your profile was successfully updated!')
-      return redirect('books:home')
+    title = "Update"
+    try:
+        profile = request.user.profile
+    except ObjectDoesNotExist:
+        profile = Profile(user=request.user)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('books:home')
+        else:
+            messages.error(request, 'Please correct the error below.')
     else:
-      messages.error(request, 'Please correct the error below.')
-  else:
-    user_form = UserForm(instance=request.user)
-    profile_form = ProfileForm(instance=profile)
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=profile)
 
-  context = {
-    'user_form': user_form,
-    'profile_form': profile_form,
-    "title": title
-  }
-  return render(request, 'profile_edit.html', context)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        "title": title
+    }
+    return render(request, 'profile_edit.html', context)
+
 
 @login_required
 def home(request):
-  return render(request, "home.html")
+    return render(request, "home.html")
+
 
 @login_required
 def profile(request):
-  return render(request, "profile.html")
+    return render(request, "profile.html")
 
 
 @login_required
@@ -138,6 +160,7 @@ def settings(request):
         'can_disconnect': can_disconnect
     })
 
+
 @login_required
 def password(request):
     if request.user.has_usable_password():
@@ -157,6 +180,7 @@ def password(request):
     else:
         form = PasswordForm(request.user)
     return render(request, 'password.html', {'form': form})
+
 
 # Test View
 
--- static/js/app/login.js
+++ static/js/app/login.js
@@ -1,13 +1,30 @@
 'use strict'
 
-function Register(event){
+function Register(event) {
     $('#registerModal').modal('toggle');
 }
 
 function RegisterUser(e) {
-    
+
 }
 
 function Login() {
-    
+    $.ajax({
+        type: "POST",
+        url: 'login/',
+        data: {
+            'username': $('#username').val(),
+            'password': $('#password').val()
+            // 'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val()
+        },
+        success: function (data) {
+            console.log(data);
+            if (data['message'] == "inactive") {
+                $("#errorlogin").html("Please verify this User Name.");
+            }
+            else {
+                $("#errorlogin").html("The User Name and Password do not match.");
+            }
+        }
+    });
 }
